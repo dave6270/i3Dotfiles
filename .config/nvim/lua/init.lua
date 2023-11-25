f5441b26ad761fcd1739465749070ef68d6bd097
dave6270 <dave6270@gmail.com>
2023-11-25 15:42:26 +0000

    Correct spelling

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,5 +1,5 @@
 -- Lets set some basic configs
 --
--- Set line numbering and use reletive line numbering
+-- Set line numbering and use relative line numbering
 vim.opt.number = true
 vim.opt.relativenumber = true
